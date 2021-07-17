0b22bcdd0e616eaaa1b8d79198574069261cbfe2
rub1nJr <rubens.cjnr@gmail.com>
2021-07-17 11:16:16 -0300

    Create treinningfunc.js

---
+++ treinningfunc.js
@@ -0,0 +1,23 @@
+function meuNome(){
+    console.log('Rubens Junior');
+}
+    var nome = meuNome()
+    console.log(nome);
+
+console.log('-------------------------------');
+
+    function nameTest(nome,idade){
+        console.log('O Nome declarado é: '+nome)
+        console.log('A idade declarada é: '+idade)
+    }
+        nameTest('Rubens', 24)
+
+console.log('-------------------------------');
+        
+    function teste2(){
+        return 'Rubão'
+    }
+        var emo = teste2()
+        console.log(emo)
+        
+console.log('-------------------------------');
