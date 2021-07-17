ed9c43544dbc48e990e9d82b6a36166b5d5255fb
rub1nJr <rubens.cjnr@gmail.com>
2021-07-17 14:13:37 -0300

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,23 @@
+function teste(){
+    console.log('Ola Mundo!')
+}
+
+teste();
+
+console.log('===========================')
+
+function testeA(parametro){
+    if(parametro == 0){
+        console.log('É o Rubão')
+    } else {
+        console.log('Não é o Rubao')
+    }
+}
+    testeA(0);
+console.log('===========================')
+
+function testeB(nome, idade){
+    console.log('Seu nome é: '+nome)
+    console.log('Sua idade é: '+idade)
+}
+    testeB('Rubens',24)
