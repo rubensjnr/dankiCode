34f9e71a095a368df69e42c66a4374369a33f2d5
rub1nJr <rubens.cjnr@gmail.com>
2021-07-17 14:35:33 -0300

    Create lacoderep.js

---
+++ lacoderep.js
@@ -0,0 +1,12 @@
+var n = 0;
+while(n < 10){
+    console.log('Contando: '+n)
+    n++
+}
+console.log('===========================')
+
+for(var c = 1; c <= 10; c++){
+    console.log('Conta aii: '+c)
+}
+console.log('===========================')
+
