d47c99023c906fd0bf14e75c983683ef3ebaf77f
rub1nJr <rubens.cjnr@gmail.com>
2021-07-17 10:54:31 -0300

    Update treino01.js

--- treino01.js
+++ treino01.js
@@ -4,7 +4,7 @@ var nome = ['Rubens','Lucas', 'Baitaka']
     nome[2] = 'Reinaldo'
     nome.push('Dotinha')
     nome.push('Felipe')
-    nome.push('Neto')
+    nome.push('Nettao')
 
 console.log(nome[0])
 console.log(nome[1])
@@ -12,3 +12,4 @@ console.log(nome[2])
 console.log(nome[3])
 console.log(nome[4])
 console.log(nome[5])
+console.log(nome)
