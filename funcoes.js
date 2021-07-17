f4c4d7b73ea0e9b5bfcf33a258d94c7ee0d8a214
rub1nJr <rubens.cjnr@gmail.com>
2021-07-17 11:00:51 -0300

    Create funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function teste(){
+    console.log('Chamando a função teste');
+}
+function teste2(nome,idade){
+    console.log ('O nome passado é: ' +nome);
+    console.log('A idade do Rubens é: '+idade);
+}
+
+teste2('Rubens',24);
+
+teste();
