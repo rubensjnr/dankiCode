ab73b0f50f17d7a79074fb4e0a4b0f42d38bc4b4
rub1nJr <rubens.cjnr@gmail.com>
2021-07-17 18:52:01 -0300

    Create objetos.js

---
+++ objetos.js
@@ -0,0 +1,22 @@
+var pessoas = {
+    'nome': 'Rubens',
+    'idade': 24,
+    'profissao': 'Assistente ADM',
+    'meta':'Programador',
+}
+console.log(`O estudante ${pessoas.nome} de ${pessoas.idade} anos de idade, tem como méta se toranar um ${pessoas.meta}, no momento, o mesmo esta trabalhando como ${pessoas.profissao}, boa sorte ${pessoas.nome}`)
+console.log('====================================')
+var jornal = [
+    {
+        'tema':'esportivo',
+        'titulo':'futebol brasileiro',
+        'conteudo':'Palmeiras',
+        'autor':'Rubens Junior',
+    },
+    {
+        'tema':'Noticias',
+        'titulo':'Economia',
+        'conteudo':'Dolar',
+        'autor':'Rubens Junior',
+    }]
+console.log('Jornal Brasil ' +jornal[0].tema)
