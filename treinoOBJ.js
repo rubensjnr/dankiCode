8438db8a030d5cccb03a56329d6d6f30b4af209f
rub1nJr <rubens.cjnr@gmail.com>
2021-07-18 21:04:55 -0300

    Create treinoOBJ.js

---
+++ treinoOBJ.js
@@ -0,0 +1,19 @@
+var person = [{
+    'nome': 'Rubens',
+    'idade': '24',
+    'profissao': 'Assistente Financeiro',
+    'curso': 'Analise e Des. Sistemas',
+    'faculdade': 'Fatec Ourinhos',
+    'periodo': 'Noturno',
+},
+{
+    'nome': 'Laciani',
+    'idade': '28',
+    'profissao': 'Gerentona foda Drogasil',
+    'curso': 'Segurança da Informação',
+    'faculdade': 'Fatec Ourinhos',
+    'periodo': 'Noturno',
+}]
+
+console.log
+
